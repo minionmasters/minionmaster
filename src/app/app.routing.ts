32715fe9dd3168928c8525cd5614ca94c8762a63
root <root@nodejs-2gb-nyc3-01.localdomain>
2017-03-26 15:26:07 +0000

    Add authentication guard to route trucks

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,7 +11,7 @@ const appRoutes: Routes = [
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'trucks', component: TruckComponent },
+    { path: 'trucks', component: TruckComponent, canActivate: [AuthGuard] },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
